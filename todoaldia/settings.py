aeb1f47ed51b13d40fd7cb674f9d1d034e50ecad
damidefe <canga2011@gmail.com>
2026-04-12 01:34:05 -0300

    cm

--- todoaldia/settings.py
+++ todoaldia/settings.py
@@ -160,3 +160,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'autos/static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+LOGIN_REDIRECT_URL = '/'
+LOGIN_URL = '/login/'
