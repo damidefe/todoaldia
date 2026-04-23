9d0ccaca3c364cad28a538b6f6d912ac9ef186d1
damidefe <canga2011@gmail.com>
2026-04-23 17:23:06 -0300

    muchos cambios gemin

--- autos/static/js/fv_editor.js
+++ autos/static/js/fv_editor.js
@@ -128,9 +128,7 @@ function fvDrawBadge(ctx, defX, defY, defSize, bgColor = null, textColor = '#fff
     ctx.fill();
     ctx.fillStyle = el.color;
     ctx.textAlign = 'center';
-    ctx.textBaseline = 'middle';
-    ctx.fillText(el.texto, tx, el.y - ph/2 + 4 + ph/2 + el.fontSize/2);
-    ctx.textBaseline = 'alphabetic';
+    ctx.fillText(el.texto, tx, el.y 0);
     return el;
 }
 
